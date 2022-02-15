3c39706016ed31d93571bfb2706a59c137e7423e
vinicius <viniciuscosta.us@gmail.com>
2022-02-15 10:59:26 -0300

    add round system to the game and README file updated.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # odin-rock-paper-and-scissors-project
 Create a rock, paper and scissors game with javascript.
+
+
+How the game works? User inputs a value between 'rock', 'paper' or 'scissor'.
+Them, the computer, using Math.random(), choose a number between 1 and 3, each nunbem is equivalent to a string value: 1 = rock, 2 = paper, 3 = scissors.  
+
+After player and computer choose their number, the system compare the values and select a winner based in the rock, paper and scissors rules.
+
+Each win is equal a one point, tie equals 0 point to both players and loss equals 1 point to the opponent.
+
+This process repeat until one of them reach three points. 
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+let playerScore = 0
+let compScore = 0
 
-for (let i = 0; i < 5; i++) {
-    // your code here!
+function playRound(playerSelection, computerSelection) {  
 
-function playRound(playerSelection, computerSelection) {
-
+    //compares player's and computer's play and determine the winner. Them add a point to playerScore or compScore.
     if (playerSelection == null || ''){
         return alert('Cancelled')
     }
@@ -16,10 +16,12 @@ function playRound(playerSelection, computerSelection) {
             playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock' ||
             playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper'
         ) {
+            playerScore++
             return `You won! ${playerSelection} beats ${computerSelection}.`
         }
         
         else {
+            compScore++
             return `You lost. ${computerSelection} beats ${playerSelection}.`
         }
     }
@@ -31,27 +33,42 @@ function playRound(playerSelection, computerSelection) {
     
   }
 
+  //loop
+  for (let i = 0; i < 1; i) { //repeat the process until one of the players reach 3 points.
 
-
-  let randomNum = (Math.floor(Math.random()*(4 - 1) + 1))
-
-  switch (randomNum) {
-
-    case 1:
-        computerPlay = 'rock'
+    if (playerScore === 3){
+        alert('You won! Congratulations')
         break;
-    case 2:
-        computerPlay = 'paper'
+    }
+    else if (compScore === 3){
+        alert('You lost!')
         break;
-    case 3:
-        computerPlay = 'scissors'
-        break;
-  }
-  
+    }
+    else{
 
-  const playerSelection = prompt('Choose between rock, paper and scissors');
-  const computerSelection = computerPlay
-  console.log(playRound(playerSelection, computerSelection));
+        let randomNum = (Math.floor(Math.random()*(4 - 1) + 1)) //This variable choose a number between 1 and 3, these numbers are converted into 'rock', 'paper' or 'scissors' values.
+
+        switch (randomNum) {
+
+            case 1:
+                computerPlay = 'rock'
+                break;
+            case 2:
+                computerPlay = 'paper'
+                break;
+            case 3:
+                computerPlay = 'scissors'
+                break;
+        }
+        
+
+        const playerSelection = prompt(`Choose between rock, paper and scissors\n Your Score: ${playerScore} | Computer Score: ${compScore} `);
+        const computerSelection = computerPlay
+        console.log(playRound(playerSelection, computerSelection)); // The playRound function, with the parameter values, executes the process of comparison of results again and again.
+        continue;
+    }
+
 }
+
 
 
